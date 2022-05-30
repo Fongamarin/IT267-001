1b2ca448b639c2a260236eecad8b583391998db1
Fongamarin <644608020014@dpu.ac.th>
2022-05-30 11:58:28 +0700

    create ligthswitch POP and OOP

---
+++ lihtswith_oop.py
@@ -0,0 +1,30 @@
+#lightswith_oop.py
+from http.client import SWITCHING_PROTOCOLS
+
+
+class Lightswith():
+    def __init__(self) -> None:
+        self.switch_stasus = False
+
+    def turnon(self):
+        self.switch_stasus = True
+   
+    def turnoff(self):
+        self.switch_stasus = False
+
+    def show(self):
+        print(f"status = {self .switch_stasus")
+
+        #สร้างวัตถุ (Object) จากแม่พิมพ์ (clss)
+        switch_obj = Lightswith()
+
+        #เรียกใช้เมธอต/ฟังชัน
+        switch_obj.show() #False
+        switch_obj.turnon()
+        switch_obj.show() #True
+        switch_obj.turnoff()
+        switch_obj.show() #False
+        switch_obj.turnoff()
+        switch_obj.show()
+        switch_obj.turnoff()
+        switch_obj.show()
