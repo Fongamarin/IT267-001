f3cea55503f6e13f618664adf2f0eb89a6a309c4
Fongamarin <644608020014@dpu.ac.th>
2022-06-14 13:49:48 +0700

    Animal

---
+++ oop1/animal.py
@@ -0,0 +1,37 @@
+class Animal:
+    #class varaiber
+    animal = "cat"
+
+    def new_animal(self,name:str,breed:str,color:str,age:int):
+        #instance variable
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.age = age
+    
+
+    def print_detail(self):
+        print(f"**********")
+        print(f"Name: {self.name}")
+        print(f"Animal: {self.animal}")
+        print(f"Breed: {self.breed}")
+        print(f"Color {self.color}")
+        print(f"Age: {self.age}")
+
+        #create object
+if __name__ == "__main__":
+        Animal.animal = "FISH"
+        ula = Animal()
+        ula.animal = "DOG"
+        ula.new_animal("ula","Scottish","white",1)
+        ula.print_detail()
+
+        drac = Animal()
+        drac.new_animal("Drac","Scottish","White",1)
+        drac.print_detail()
+        drac.breed = "Catfish"
+        drac.print_detail()
+
+        #เรียกดูข้อมูลของ opject ผ่านทางชื่อclass
+        Animal.print_detail(ula) #ula.print_detail()
+        Animal.print_detail(drac) #drac.print_detail()
--- oop1/person.py
+++ oop1/person.py
@@ -1,33 +1,49 @@
-class Person :
+class Person:
+    country = "Thailand" #class variable
     def __init__(self,name,gender,profession,study) -> None:
+        #instance  variables
         self.name = name
         self.gender = gender
         self.profession = profession
         self.study2 = study
-    
+
 
     def work(self):
-        print(f"{self.name} is workig as a{self.profession}")
+        print(f"{self.name} is working as a {self.profession}")
 
     def study(self):
-        print(f"{self.name} studies for {self.study2}hour(s)")
-        
-    def show(self):
-        print(f"Name:{self.name} Gender: {self.gender} perfession : {self.profession} study : {self.study2}")
+        print(f"{self.name} studies for {self.study2} hour(s)")
 
-    #create object
-jessa_obj= Person ("Jessa","Male","Software Engineer",10)
+    def show(self):
+        print(f"Name: {self.name} Gender: {self.gender} Profession: {self.profession} Study: {self.study2}")
+
+#create object
+jessa_obj = Person ("Jess","Male","Software Engineer",10)
 jessa_obj.show()
 jessa_obj.work()
 jessa_obj.study()
 
-jon_obj= Person ("Jon","Male","doctor",15)
+jon_obj = Person ("Jon","Male","Doctor",15)
 jon_obj.show()
 jon_obj.work()
 jon_obj.study()
 
-lalisa_obj= Person ("lalisa","Male","doctor",13)
+lalisa_obj = Person ("Lalisa","Female","Korean Singer",13)
 lalisa_obj.work()
 
 
+print(f"Class Variable: {Person.country}")
+print(f"Instance Variables: {lalisa_obj.country}")
 
+#assign value
+lalisa_obj.country = "Korea"
+print("-------")
+print(f"Class Variable: {Person.country}")
+print(f"Instance Variables: {lalisa_obj.country}")
+print(f"Instance Variables: {jon_obj.country}")
+
+# assign class variable
+Person.country = "England"
+print(f"Class Variable: {Person.country}")
+print(f"Instance Variables: {lalisa_obj.country}")
+print(f"Instance Variables: {jon_obj.country}")
